4a6aaf367474f8dc442314100a2166e04a29dd20
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-17 09:35:29 -0300

    chore: initial project setup

---
+++ src/test/java/io/github/Guimaraes131/logistics_api/LogisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.logistics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

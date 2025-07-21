5df97ef9e491bd7a11bef299e6ea0802918a7789
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-21 10:18:03 -0300

    feat(recipient): add findByCpf method that returns an Optional<Recipient>

--- src/main/java/io/github/Guimaraes131/logistics_api/repository/RecipientRepository.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/repository/RecipientRepository.java
@@ -3,7 +3,10 @@ package io.github.Guimaraes131.logistics_api.repository;
 import io.github.Guimaraes131.logistics_api.model.Recipient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RecipientRepository extends JpaRepository<Recipient, UUID> {
+
+    Optional<Recipient> findByCpf(String cpf);
 }

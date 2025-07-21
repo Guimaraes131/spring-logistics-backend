d6d2450f628e4ac6d562baf74b898a8e8e8f362b
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-21 13:38:14 -0300

    feat(recipient): add validateAddressExists method

--- src/main/java/io/github/Guimaraes131/logistics_api/validator/RecipientValidator.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/validator/RecipientValidator.java
@@ -1,8 +1,11 @@
 package io.github.Guimaraes131.logistics_api.validator;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.PostRecipientDTO;
 import io.github.Guimaraes131.logistics_api.exception.DuplicateRecordException;
 import io.github.Guimaraes131.logistics_api.model.Recipient;
+import io.github.Guimaraes131.logistics_api.repository.AddressRepository;
 import io.github.Guimaraes131.logistics_api.repository.RecipientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +15,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RecipientValidator {
 
-    private final RecipientRepository repository;
+    private final RecipientRepository recipientRepository;
+    private final AddressRepository addressRepository;
 
     public void validate(Recipient recipient) {
         if (existsRecipient(recipient)) {
@@ -20,8 +24,14 @@ public class RecipientValidator {
         }
     }
 
+    public void validateAddressExists(PostRecipientDTO dto) {
+        if (!addressRepository.existsById(dto.addressId())) {
+            throw new EntityNotFoundException("Address does not exist");
+        }
+    }
+
     private boolean existsRecipient(Recipient recipient) {
-        Optional<Recipient> entity = repository.findByCpf(recipient.getCpf());
+        Optional<Recipient> entity = recipientRepository.findByCpf(recipient.getCpf());
 
         if (recipient.getId() == null && entity.isPresent()) {
             return true;

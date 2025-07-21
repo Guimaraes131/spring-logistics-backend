bf3e716dc7debeca29653c4b0a2b431e6bec2860
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-21 13:39:59 -0300

    feat(recipient): inject validator, mapper. Validate in create method and return Optional<GetRecipientDTO> in get method

--- src/main/java/io/github/Guimaraes131/logistics_api/service/RecipientService.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/service/RecipientService.java
@@ -1,5 +1,8 @@
 package io.github.Guimaraes131.logistics_api.service;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.GetRecipientDTO;
+import io.github.Guimaraes131.logistics_api.controller.dto.PostRecipientDTO;
+import io.github.Guimaraes131.logistics_api.controller.mapper.RecipientMapper;
 import io.github.Guimaraes131.logistics_api.model.Recipient;
 import io.github.Guimaraes131.logistics_api.repository.RecipientRepository;
 import io.github.Guimaraes131.logistics_api.validator.RecipientValidator;
@@ -15,14 +18,21 @@ public class RecipientService {
 
     private final RecipientRepository repository;
     private final RecipientValidator validator;
+    private final RecipientMapper mapper;
 
-    public void create(Recipient recipient) {
+
+    public Recipient create(PostRecipientDTO dto) {
+        Recipient recipient = mapper.toEntity(dto);
+
+        validator.validateAddressExists(dto);
         validator.validate(recipient);
-        repository.save(recipient);
+
+        return repository.save(recipient);
     }
 
-    public Optional<Recipient> get(UUID id) {
-        return repository.findById(id);
+    public Optional<GetRecipientDTO> get(UUID id) {
+        return repository.findById(id)
+                .map(mapper::toDTO);
     }
 
     public void delete(UUID id) {

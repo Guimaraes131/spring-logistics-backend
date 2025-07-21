cf92b6bf4cd7d30e968508bd5480f26f2c648453
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-21 10:12:45 -0300

    feat(exception): add exception package with DuplicateRecordException

---
+++ src/main/java/io/github/Guimaraes131/logistics_api/exception/DuplicateRecordException.java
@@ -0,0 +1,7 @@
+package io.github.Guimaraes131.logistics_api.exception;
+
+public class DuplicateRecordException extends RuntimeException {
+    public DuplicateRecordException(String message) {
+        super(message);
+    }
+}

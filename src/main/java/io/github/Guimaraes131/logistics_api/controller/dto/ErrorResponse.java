1efa75c1bc591b61c6979907c4b774094ac1a24d
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-21 10:15:50 -0300

    feat(error-response): add static method that returns a standardized conflict ErrorResponse

--- src/main/java/io/github/Guimaraes131/logistics_api/controller/dto/ErrorResponse.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/dto/ErrorResponse.java
@@ -1,6 +1,12 @@
 package io.github.Guimaraes131.logistics_api.controller.dto;
 
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 public record ErrorResponse(Integer status, String message, List<FieldError> errors) {
+
+    public static ErrorResponse conflictResponse(String message) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), message, List.of());
+    }
 }

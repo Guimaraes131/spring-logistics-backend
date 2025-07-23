4a1cf3d0815afc2bed21697e5ca70d5c8748dc10
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-23 20:05:49 -0300

    feat(delivery): add create method and inject DeliveryMapper

--- src/main/java/io/github/Guimaraes131/logistics_api/service/DeliveryService.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/service/DeliveryService.java
@@ -1,12 +1,33 @@
 package io.github.Guimaraes131.logistics_api.service;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.PostDeliveryDTO;
+import io.github.Guimaraes131.logistics_api.controller.mapper.DeliveryMapper;
+import io.github.Guimaraes131.logistics_api.model.Delivery;
+import io.github.Guimaraes131.logistics_api.model.enums.Status;
 import io.github.Guimaraes131.logistics_api.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
 public class DeliveryService {
 
     private final DeliveryRepository repository;
+    private final DeliveryMapper mapper;
+
+    public Delivery create(PostDeliveryDTO dto) {
+        var entity = mapper.toEntity(dto);
+        entity.setStatus(Status.PENDING);
+        entity.setTrackingCode(
+                String.valueOf(
+                        ThreadLocalRandom
+                                .current()
+                                .nextInt(10000000, 100000000)
+                )
+        );
+
+        return repository.save(entity);
+    }
 }

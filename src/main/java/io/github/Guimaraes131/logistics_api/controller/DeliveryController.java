913cf292434e7d70d318bfe0ed1d67008b0645d9
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-23 20:05:11 -0300

    feat(delivery): implement POST endpoint

--- src/main/java/io/github/Guimaraes131/logistics_api/controller/DeliveryController.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/DeliveryController.java
@@ -1,7 +1,11 @@
 package io.github.Guimaraes131.logistics_api.controller;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.PostDeliveryDTO;
 import io.github.Guimaraes131.logistics_api.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeliveryController implements GenericController {
 
     private final DeliveryService service;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody PostDeliveryDTO dto) {
+        var entity = service.create(dto);
+
+        return ResponseEntity.created(generateLocationHeader(entity.getId())).build();
+    }
 }

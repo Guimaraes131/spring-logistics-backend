a05e76b4a86f827ceeae557dc0e49d7399c52592
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 05:52:53 -0300

    feat(delivery): implement output DTO for GET endpoint

---
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/dto/GetDeliveryDTO.java
@@ -0,0 +1,16 @@
+package io.github.Guimaraes131.logistics_api.controller.dto;
+
+import io.github.Guimaraes131.logistics_api.model.enums.Status;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record GetDeliveryDTO(
+        UUID id,
+        String trackingCode,
+        Status status,
+        LocalDateTime creationDate,
+        LocalDateTime deliveryDate,
+        GetRecipientDTO recipient
+) {
+}

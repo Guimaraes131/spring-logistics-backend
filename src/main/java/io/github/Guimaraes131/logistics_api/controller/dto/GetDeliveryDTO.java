0facd9aef895c55f6c6492b456d26700f3c502f5
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 18:31:47 -0300

    feat(delivery): add products field

--- src/main/java/io/github/Guimaraes131/logistics_api/controller/dto/GetDeliveryDTO.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/dto/GetDeliveryDTO.java
@@ -3,6 +3,7 @@ package io.github.Guimaraes131.logistics_api.controller.dto;
 import io.github.Guimaraes131.logistics_api.model.enums.Status;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record GetDeliveryDTO(
@@ -11,6 +12,7 @@ public record GetDeliveryDTO(
         Status status,
         LocalDateTime creationDate,
         LocalDateTime deliveryDate,
-        GetRecipientDTO recipient
+        GetRecipientDTO recipient,
+        List<GetProductDTO> products
 ) {
 }

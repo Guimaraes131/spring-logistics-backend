bccf9bf2243539052f9b85d37ce77c33da6bc4ea
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 18:51:28 -0300

    feat(product): add findAllByDeliveryTrackingCode method

--- src/main/java/io/github/Guimaraes131/logistics_api/repository/ProductRepository.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/repository/ProductRepository.java
@@ -3,7 +3,10 @@ package io.github.Guimaraes131.logistics_api.repository;
 import io.github.Guimaraes131.logistics_api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    List<Product> findAllByDeliveryTrackingCode(String code);
 }

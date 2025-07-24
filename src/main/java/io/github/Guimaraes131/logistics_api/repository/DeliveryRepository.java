9e0181918458ffb320fa2da35d566f7a28de6e6d
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 06:06:33 -0300

    feat(delivery): add findByRecipientCpf method

--- src/main/java/io/github/Guimaraes131/logistics_api/repository/DeliveryRepository.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/repository/DeliveryRepository.java
@@ -3,10 +3,12 @@ package io.github.Guimaraes131.logistics_api.repository;
 import io.github.Guimaraes131.logistics_api.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
 
     Optional<Delivery> findByTrackingCode(String code);
+    List<Delivery> findByRecipientCpf(String cpf);
 }

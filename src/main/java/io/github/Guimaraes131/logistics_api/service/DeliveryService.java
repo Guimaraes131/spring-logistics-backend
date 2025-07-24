bd1a96cb491ded74064ee26a84d084c08ce32d9f
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 09:06:07 -0300

    feat(delivery): add updateStatus method

--- src/main/java/io/github/Guimaraes131/logistics_api/service/DeliveryService.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/service/DeliveryService.java
@@ -2,10 +2,12 @@ package io.github.Guimaraes131.logistics_api.service;
 
 import io.github.Guimaraes131.logistics_api.controller.dto.GetDeliveryDTO;
 import io.github.Guimaraes131.logistics_api.controller.dto.PostDeliveryDTO;
+import io.github.Guimaraes131.logistics_api.controller.dto.UpdateDeliveryStatusDTO;
 import io.github.Guimaraes131.logistics_api.controller.mapper.DeliveryMapper;
 import io.github.Guimaraes131.logistics_api.model.Delivery;
 import io.github.Guimaraes131.logistics_api.model.enums.Status;
 import io.github.Guimaraes131.logistics_api.repository.DeliveryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +38,10 @@ public class DeliveryService {
         return repository.save(entity);
     }
 
+    public Optional<Delivery> get(UUID id) {
+        return repository.findById(id);
+    }
+
     public Optional<GetDeliveryDTO> getByTrackingCode(String code) {
         return repository.findByTrackingCode(code)
                 .map(mapper::toDTO);
@@ -50,5 +56,13 @@ public class DeliveryService {
 
     public void delete(UUID id) {
         repository.deleteById(id);
+    }
+
+    public void updateStatus(UUID id, UpdateDeliveryStatusDTO dto) {
+        repository.findById(id)
+                .map(delivery -> {
+                    delivery.setStatus(dto.status());
+                    return repository.save(delivery);
+                }).orElseThrow(() -> new EntityNotFoundException("Delivery does not exist"));
     }
 }

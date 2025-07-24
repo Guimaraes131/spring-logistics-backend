5c25c1bbc1c34bc33ec5df5a4425103d564fe3a1
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 13:04:23 -0300

    feat(exception): add InvalidStatusException

---
+++ src/main/java/io/github/Guimaraes131/logistics_api/exception/InvalidStatusException.java
@@ -0,0 +1,7 @@
+package io.github.Guimaraes131.logistics_api.exception;
+
+public class InvalidStatusException extends RuntimeException {
+    public InvalidStatusException(String message) {
+        super(message);
+    }
+}

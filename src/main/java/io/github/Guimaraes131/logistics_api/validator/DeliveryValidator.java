f28b1a3cd59361c9a8f3cc8d9a7eb031e296e92a
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 13:06:40 -0300

    feat(delivery): overload validate method to accept update validation

--- src/main/java/io/github/Guimaraes131/logistics_api/validator/DeliveryValidator.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/validator/DeliveryValidator.java
@@ -1,11 +1,16 @@
 package io.github.Guimaraes131.logistics_api.validator;
 
 import io.github.Guimaraes131.logistics_api.controller.dto.PostDeliveryDTO;
+import io.github.Guimaraes131.logistics_api.controller.dto.UpdateDeliveryStatusDTO;
+import io.github.Guimaraes131.logistics_api.exception.InvalidStatusException;
+import io.github.Guimaraes131.logistics_api.model.enums.Status;
 import io.github.Guimaraes131.logistics_api.repository.RecipientRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.EnumSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Component
@@ -17,6 +22,19 @@ public class DeliveryValidator {
     public void validate(PostDeliveryDTO dto) {
         if (!existsRecipient(dto.recipientId())) {
             throw new EntityNotFoundException("Recipient does not exist");
+        }
+    }
+
+    public void validate(UpdateDeliveryStatusDTO dto) {
+        Set<Status> statusList = EnumSet.of(
+                Status.PENDING,
+                Status.DELIVERED,
+                Status.IN_TRANSIT,
+                Status.DISPATCHED
+        );
+
+        if (dto.status() == null || !statusList.contains(dto.status())) {
+            throw new InvalidStatusException("Invalid Status");
         }
     }
 

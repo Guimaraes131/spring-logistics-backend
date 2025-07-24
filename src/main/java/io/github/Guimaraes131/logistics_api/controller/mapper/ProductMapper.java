33f4c43dcc191e7be38e99de1044c586c12c1544
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 18:02:24 -0300

    feat(product): introduce mapper with toEntity method

---
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/mapper/ProductMapper.java
@@ -0,0 +1,18 @@
+package io.github.Guimaraes131.logistics_api.controller.mapper;
+
+import io.github.Guimaraes131.logistics_api.controller.dto.PostProductDTO;
+import io.github.Guimaraes131.logistics_api.model.Product;
+import io.github.Guimaraes131.logistics_api.repository.DeliveryRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring", uses = DeliveryMapper.class)
+public abstract class ProductMapper {
+
+    @Autowired
+    DeliveryRepository repository;
+
+    @Mapping(target = "delivery", expression = "java( repository.findById(dto.deliveryId()).orElse(null) )")
+    public abstract Product toEntity(PostProductDTO dto);
+}

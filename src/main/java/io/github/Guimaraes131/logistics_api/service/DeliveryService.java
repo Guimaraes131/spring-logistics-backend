0bc87a71f290eed0e78d836f60fcd03c289a74a5
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 05:54:16 -0300

    feat(delivery): add getByTrackingCode method

--- src/main/java/io/github/Guimaraes131/logistics_api/service/DeliveryService.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/service/DeliveryService.java
@@ -1,5 +1,6 @@
 package io.github.Guimaraes131.logistics_api.service;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.GetDeliveryDTO;
 import io.github.Guimaraes131.logistics_api.controller.dto.PostDeliveryDTO;
 import io.github.Guimaraes131.logistics_api.controller.mapper.DeliveryMapper;
 import io.github.Guimaraes131.logistics_api.model.Delivery;
@@ -8,6 +9,7 @@ import io.github.Guimaraes131.logistics_api.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
@@ -29,5 +31,10 @@ public class DeliveryService {
         );
 
         return repository.save(entity);
+    }
+
+    public Optional<GetDeliveryDTO> getByTrackingCode(String code) {
+        return repository.findByTrackingCode(code)
+                .map(mapper::toDTO);
     }
 }

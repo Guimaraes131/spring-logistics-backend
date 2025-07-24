2338e05b9fe157c67cb27effba84503231c3dbaa
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 09:05:33 -0300

    feat(delivery): introduce input DTO for PATCH endpoint

---
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/dto/UpdateDeliveryStatusDTO.java
@@ -0,0 +1,6 @@
+package io.github.Guimaraes131.logistics_api.controller.dto;
+
+import io.github.Guimaraes131.logistics_api.model.enums.Status;
+
+public record UpdateDeliveryStatusDTO(Status status) {
+}

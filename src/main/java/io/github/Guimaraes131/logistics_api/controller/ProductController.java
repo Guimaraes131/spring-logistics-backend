c1d0df07585f2e6763f12c350c3e428f088a7537
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 18:03:07 -0300

    feat(product): map POST endpoint

--- src/main/java/io/github/Guimaraes131/logistics_api/controller/ProductController.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/controller/ProductController.java
@@ -1,14 +1,25 @@
 package io.github.Guimaraes131.logistics_api.controller;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.PostProductDTO;
 import io.github.Guimaraes131.logistics_api.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
-public class ProductController {
+public class ProductController implements GenericController {
 
     private final ProductService service;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody PostProductDTO dto) {
+        var entity = service.create(dto);
+
+        return ResponseEntity.created(generateLocationHeader(entity.getId())).build();
+    }
 }

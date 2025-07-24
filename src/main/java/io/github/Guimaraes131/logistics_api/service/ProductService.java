b8fcbd6de2bbddcf02c858fabff388eab72325c0
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-24 18:02:45 -0300

    feat(product): add create method returning Product

--- src/main/java/io/github/Guimaraes131/logistics_api/service/ProductService.java
+++ src/main/java/io/github/Guimaraes131/logistics_api/service/ProductService.java
@@ -1,5 +1,8 @@
 package io.github.Guimaraes131.logistics_api.service;
 
+import io.github.Guimaraes131.logistics_api.controller.dto.PostProductDTO;
+import io.github.Guimaraes131.logistics_api.controller.mapper.ProductMapper;
+import io.github.Guimaraes131.logistics_api.model.Product;
 import io.github.Guimaraes131.logistics_api.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,11 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private final ProductRepository repository;
+    private final ProductMapper mapper;
+
+    public Product create(PostProductDTO dto) {
+        var entity = mapper.toEntity(dto);
+
+        return repository.save(entity);
+    }
 }
